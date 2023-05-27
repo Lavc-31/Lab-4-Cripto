c2d84f170fbadd4b98ba67df267dc8aa9d83fcd8
Lavc-31 <88551937+Lavc-31@users.noreply.github.com>
2023-05-27 00:51:09 -0400

    Update LAB4-Cripto.user.js

--- LAB4-Cripto.user.js
+++ LAB4-Cripto.user.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://cripto.tiiny.site/
+// @match        *://*/*
 // @run-at       document-end
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
 // @grant        none
